eb19c4273afc644e36e0c987c7306d134857a3c1
BuriticaSebas <jhoseburitica40@gmail.com>
2024-08-14 21:28:48 -0500

    tal cual como se entrego con el avance capstone

---
+++ src/Main.java
@@ -0,0 +1,138 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int valorWidth = capturarAncho(args);
+        int valorH = capturarAlto(args);
+        int valorG = capturarGeneracion(args);
+        int valorV = capturarVelocidad(args);
+        String[] patron = capturarPatron(args);
+        int valorVe = capturarVecindario(args);
+
+
+        // Mostrar resultados, manejando errores si es necesario
+        System.out.println("w= " + mostrarError(valorWidth));
+        System.out.println("h= " + mostrarError(valorH));
+        System.out.println("g=500 " + mostrarError(valorG) );
+        System.out.println("s= " +  mostrarError(valorV));
+        System.out.println("p= "+ Arrays.toString(patron));
+        System.out.println("n="+ mostrarError(valorVe));
+        if (valorWidth != 24847 && valorWidth != 34857 &&
+                valorH != 24847 && valorH != 34857 &&
+                valorG != 24847 && valorG != 34857 &&
+                valorV != 24847 && valorV != 34857) {
+            mostrarPatron(patron, valorWidth, valorH);
+        }
+
+    }
+
+    public static String mostrarError(int valor) {
+        if (valor == 24847) {
+            return "INVALIDO";
+        } else if (valor == 34857) {
+            return "NO PRESENTE";
+        }
+        return String.valueOf(valor); // Si no hay error, retorna el valor como String
+    }
+
+    public static String limpiarDatos(String datos) {
+        return datos.replace("p=", "").replace("\"", "").replaceAll("\\s+", "");
+    }
+
+    public static int capturarAncho(String[] args) {
+        try {
+            String width = args[0];
+            String valueW = width.replaceAll("[w=]", "");
+            if (!valueW.equals("10") && !valueW.equals("20") && !valueW.equals("40") && !valueW.equals("80")) {
+                throw new NumberFormatException("Valor de ancho no válido: " + valueW);
+            }
+            return Integer.parseInt(valueW);
+        } catch (NumberFormatException error) {
+            return 24847; // Valor especial para "INVALIDO"
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return 34857; // Valor especial para "NO PRESENTE"
+        }
+    }
+
+    public static int capturarAlto(String[] args) {
+        try {
+            String height = args[1];
+            String valueH = height.replaceAll("[h=]", "");
+            if (!valueH.equals("10") && !valueH.equals("20") && !valueH.equals("40")) {
+                throw new NumberFormatException("Valor de ancho no válido: " + valueH);
+            }
+
+            return Integer.parseInt(valueH);
+        } catch (NumberFormatException error) {
+            return 24847; // O usar otro código de error si es necesario
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return 34857; // O usar otro código de error si es necesario
+        }
+    }
+
+    public static int capturarGeneracion(String[] args) {
+        try {
+            String generation = args[2];
+            String valueG = generation.replaceAll("[g=]", "");
+            return Integer.parseInt(valueG);
+        } catch (NumberFormatException error) {
+            return 24847; // O usar otro código de error si es necesario
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return 34857; // O usar otro código de error si es necesario
+        }
+    }
+
+    public static int capturarVelocidad(String[] args) {
+        try {
+            String velocidad = args[3];
+            String valueV = velocidad.replaceAll("[s=]", "");
+            return Integer.parseInt(valueV);
+        } catch (NumberFormatException error) {
+            return 24847; // O usar otro código de error si es necesario
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return 34857; // O usar otro código de error si es necesario
+        }
+    }
+
+    public static int capturarVecindario(String[] args) {
+        try {
+            String vecindario = args[5];
+            String valueVe = vecindario.replaceAll("[n=]", "");
+            return Integer.parseInt(valueVe);
+        }catch (NumberFormatException error) {
+            return 3; // O usar otro código de error si es necesario
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return 3; // O usar otro código de error si es necesario
+        }
+    }
+
+    public static String[] capturarPatron(String[] args) {
+        try {
+            String wFormatString = args[4];
+            String wFormatStringLimpiado = limpiarDatos(wFormatString);
+            return wFormatStringLimpiado.split("#");
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return new String[]{"Error"}; // O retorna un array vacío o con un mensaje de error
+        }
+    }
+
+    public static void mostrarPatron(String[] patrones, int fila, int columna) {
+        int[][] tablero = new int[fila][columna];
+        int contadorFilas = 0;
+        int contadorColumnas = 0;
+        for (String fil : patrones) {
+            for (int i = 0; i < fil.length(); i++) {
+                int valor = Integer.parseInt(fil.substring(i, i + 1));
+                tablero[contadorFilas][contadorColumnas] = valor;
+                contadorColumnas++;
+            }
+            contadorFilas++;
+            contadorColumnas = 0;
+        }
+        for (int[] filaTablero : tablero) {
+            System.out.println(Arrays.toString(filaTablero));
+        }
+    }
+}
+
+
